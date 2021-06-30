ff784945f89fcbfab8eb4973ef691320814f5021
Abdullah <a.alqattan@mail.com>
2021-06-30 15:20:24 +0300

    added .env

--- backend/test_flaskr.py
+++ backend/test_flaskr.py
@@ -6,6 +6,12 @@ from flask_sqlalchemy import SQLAlchemy
 from flaskr import create_app
 from models import setup_db, Question, Category
 
+from dotenv import load_dotenv, find_dotenv
+
+load_dotenv(find_dotenv())
+
+DATABASE_PATH = os.environ.get("DATABASE_PATH")
+
 
 class TriviaTestCase(unittest.TestCase):
     """This class represents the trivia test case"""
@@ -15,7 +21,7 @@ class TriviaTestCase(unittest.TestCase):
         self.app = create_app()
         self.client = self.app.test_client
         self.database_name = "trivia_test"
-        self.database_path = "postgresql://{}@{}/{}".format('postgres:1342','localhost:5432', self.database_name)
+        self.database_path = DATABASE_PATH
         setup_db(self.app, self.database_path)
 
         # binds the app to the current context
